45f2ef614e176bd9e9f6b2bc320283eb8ffc167d
kadima <williamsvgw@gmail.com>
2024-12-09 22:15:14 +0100

    strategy payment process example

---
+++ src/main/java/org/example/strategy/payment_example/PaymentStrategy.java
@@ -0,0 +1,5 @@
+package org.example.strategy.payment_example;
+
+public interface PaymentStrategy {
+  public void pay(int amount);
+}
---
+++ src/main/java/org/example/strategy/payment_example/PaypalStrategy.java
@@ -0,0 +1,16 @@
+package org.example.strategy.payment_example;
+
+public class PaypalStrategy implements PaymentStrategy{
+  private String emailId;
+  private String password;
+
+  public PaypalStrategy(String email, String pwd){
+    this.emailId = email;
+    this.password = pwd;
+  }
+
+  @Override
+  public void pay(int amount) {
+    System.out.println(amount + "Paid using paypal");
+  }
+}
