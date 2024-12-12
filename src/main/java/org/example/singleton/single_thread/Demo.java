9b3b79caae19460f9a88f37e819c2c70ba0a070c
kadima <williamsvgw@gmail.com>
2024-12-12 22:56:20 +0100

    singleton single-thread example

---
+++ src/main/java/org/example/singleton/single_thread/Demo.java
@@ -0,0 +1,14 @@
+package org.example.singleton.single_thread;
+
+public class Demo {
+  public static void main(String[] args) {
+    System.out.println("If you see the same value, then singleton was reused (yay!)" + "\n" +
+            "If you see different values, then 2 singletons were created (booo!!)" + "\n\n" +
+            "RESULT:" + "\n");
+
+    Singleton s = Singleton.getInstance("FOO");
+    Singleton s2 = Singleton.getInstance("BAR");
+    System.out.println(s.value);
+    System.out.println(s2.value);
+  }
+}
